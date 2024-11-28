fc77a148fb821f3068af2757613a0420c07f3d88
Rahamtullah sheikh <76489436+rhm856591@users.noreply.github.com>
2024-11-29 00:44:14 +0530

    fixed contact api

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -15,10 +15,10 @@ export async function POST(req, res) {
       const newContact = await Contact({ name, email, message });
       await newContact.save();
 
-      NextResponse.json({ success: true, data: newContact });
+      // NextResponse.json({ success: true, data: newContact });
       return NextResponse.json({ message: 'Form submitted successfully!' }, { status: 200 });
     } catch (error) {
-      NextResponse.json({ success: false, error: error.message });
+      // NextResponse.json({ success: false, error: error.message });
       return NextResponse.json({ message: 'An error occurred. Please try again later.' }, { status: 500 });
     }
 }
